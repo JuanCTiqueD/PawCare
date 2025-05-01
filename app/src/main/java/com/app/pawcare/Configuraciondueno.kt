f2275caf8e5183a985fe4aa93db031ace6993d35
Valencaicedo <valentina.caicedos@hotmail.com>
2025-05-01 15:53:35 -0500

    Vista perfil dueño, configuracion y editar perfil

--- app/src/main/java/com/app/pawcare/Configuraciondueno.kt
+++ app/src/main/java/com/app/pawcare/Configuraciondueno.kt
@@ -1,6 +1,9 @@
 package com.app.pawcare
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,26 @@ class Configuraciondueno : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_configuraciondueno)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // ✅ Regresar a PerfilDuenoActivity
+        val imageBack = findViewById<ImageView>(R.id.imageView36)
+        imageBack.setOnClickListener {
+            val intent = Intent(this, PerfilDuenoActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        // ✅ Ir a EditarPerfilDueno
+        val editarPerfil = findViewById<TextView>(R.id.editarperfil)
+        editarPerfil.setOnClickListener {
+            val intent = Intent(this, EditarPerfilDueno::class.java)
+            startActivity(intent)
         }
     }
 }
