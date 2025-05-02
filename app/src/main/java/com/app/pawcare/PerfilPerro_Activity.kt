e581a3b1d3180a03819c825d74ca15aa707e98a4
Leo <leonardo2005leojuan@gmail.com>
2025-05-01 23:16:21 -0500

    calendario

--- app/src/main/java/com/app/pawcare/PerfilPerro_Activity.kt
+++ app/src/main/java/com/app/pawcare/PerfilPerro_Activity.kt
@@ -1,16 +1,39 @@
 package com.app.pawcare
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Calendar
 
 class PerfilPerro_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_perfil_perro)
+
+        val fechaNacimiento = findViewById<EditText>(R.id.Fechanacer)
+        fechaNacimiento.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(
+                this,
+                { _, selectedYear, selectedMonth, selectedDay ->
+                    val fecha = "${selectedDay}/${selectedMonth + 1}/${selectedYear}"
+                    fechaNacimiento.setText(fecha)
+                },
+                year, month, day
+            )
+
+            datePickerDialog.show()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
